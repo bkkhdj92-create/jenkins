8e8805349463147651c9babc7d6b27f2a69ece06
khadija <bkkhdj92@gmail.com>
2026-05-19 19:31:01 +0100

    first job

--- hello.py
+++ hello.py
@@ -2,9 +2,10 @@ print("=" * 40)
 print("Bienvenue dans mon premier job Jenkins !")
 print("=" * 40)
 
-nom = input("Quel est ton nom ?")
-print(f"Bonjour {nom}, ton job Jenkins a reussi !")
+nom = input("Quel est ton nom ? ")
+print(f"Bonjour {nom}, ton job Jenkins a réussi !")
 
-a=10 
-b=5
+a = 10
+b = 5
 print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
